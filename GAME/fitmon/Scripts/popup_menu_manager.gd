e32d4532ef471e18278d743b59f2428cf7aef045
KartikeyCode <codekartikey@gmail.com>
2025-02-27 09:34:12 +0530

    init game

---
+++ GAME/fitmon/Scripts/popup_menu_manager.gd
@@ -0,0 +1,18 @@
+extends MarginContainer
+
+@export var menu_screen: VBoxContainer
+@export var open_menu_screen: VBoxContainer
+
+func _ready() -> void:
+	menu_screen.visible = false
+
+func toggle_visibility(object):
+	if object.visible:
+		object.visible = false
+	else:
+		object.visible = true
+
+
+func _on_toggle_menu_button_pressed() -> void:
+	toggle_visibility(menu_screen)
+	toggle_visibility(open_menu_screen)
